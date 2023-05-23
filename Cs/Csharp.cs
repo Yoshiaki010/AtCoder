145fb14f69db3027d41b522fb20830f3aa328cd2
Yoshiaki <22211370yy@tama.ac.jp>
2023-05-23 16:34:33 +0900

    Update

--- Cs/Csharp.cs
+++ Cs/Csharp.cs
@@ -1,13 +1,18 @@
 using System;
 public class Program{
     public static void Main(){
-      Console.WriteLine("打ち上げたい回数を入力し、ENTERキーを入力してください。");
-      int num = Console.ReadLine();
-      Console.Write(num);
-      num = int.Parse(num);
-//      for (int i = 0; i<=num;i++){
-  //      Console.Write(i);
-    //    Console.Write("回目の花火：＊");
-      //}
+//以下の２行は含めない
+        var exStdIn = new System.IO.StreamReader("stdin.txt");
+        System.Console.SetIn(exStdIn);
+
+        Console.WriteLine("打ち上げたい回数を入力し、ENTERキーを入力してください。");
+        int num = Console.ReadLine();
+        Console.Write(num);
+        num = int.Parse(num);
+
+        for (int i = 0; i<=num;i++){
+            Console.Write(i);
+            Console.Write("回目の花火：＊");
+        }
     }
 }
