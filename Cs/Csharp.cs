c4363915f3fad8f54ec3f57e818ef98d52866def
Yoshiaki <22211370yy@tama.ac.jp>
2023-05-23 16:16:28 +0900

    Update

---
+++ Cs/Csharp.cs
@@ -0,0 +1,13 @@
+using System;
+public class Program{
+    public static void Main(){
+      Console.WriteLine("打ち上げたい回数を入力し、ENTERキーを入力してください。");
+      int num = Console.ReadLine();
+      Console.Write(num);
+      num = int.Parse(num);
+//      for (int i = 0; i<=num;i++){
+  //      Console.Write(i);
+    //    Console.Write("回目の花火：＊");
+      //}
+    }
+}
