7955f5c50532ac430359c8887a1b50cabf1bdb93
Yoshi01O <22211370yy@tama.ac.jp>
2023-05-28 23:07:34 +0900

    Update

---
+++ Py/2023_0528.py
@@ -0,0 +1,31 @@
+'''
+#lv3
+n=int(input())
+a=list(map(int,input().split()))
+ans="Yes"
+for i in range(n):
+    if i%2!=0 and ans=="Yes":
+        if a[i-1]<a[i] and a[i]>a[i+1]:
+            ans="Yes"
+        else:
+            ans="No"
+if ans=="Yes":
+    print(ans)
+else:
+    rea=sorted(a,reverse=True)
+    big=[]
+    small=[]
+    a=[]
+    for i in range((n-1)//2):
+        a+=[rea[i+((n-1)//2)]]
+        a+=[rea[i]]
+    a+=[rea[-1]]
+    ans="Yes"
+    for i in range(n):
+        if i%2!=0 and ans=="Yes":
+            if a[i-1]<a[i] and a[i]>a[i+1]:
+                ans="Yes"
+            else:
+                ans="No"
+    print(ans)
+'''
