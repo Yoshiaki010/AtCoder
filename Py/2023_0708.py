e182547208f2a5a6074c9d1a8a506ad396e1c440
Yoshi01O <22211370yy@tama.ac.jp>
2023-07-08 23:07:15 +0900

    lv3わからんぬ～

---
+++ Py/2023_0708.py
@@ -0,0 +1,54 @@
+"""
+#lv1
+a,b=list(map(int,input().split()))
+ans="No"
+if a % 3 != 0:
+    if a+1 == b:
+        ans="Yes"
+print(ans)
+"""
+"""
+#lv2
+n=int(input())
+b=[]
+ans=[]
+for i in range(n):
+    b+=[list(map(int,input()))]
+
+for i in range(n):
+    if i == 0:
+        ans+=[[b[1][0]]+b[0][:n-1]]
+    elif i == n-1:
+        ans+=[b[n-1][1:n]+[b[n-2][n-1]]]
+    else:
+        ans+=[[b[i+1][0]]+b[i][1:n-1]+[b[i-1][n-1]]]
+for i in ans:
+    for j in i:
+        print(j,end="")
+    print()
+"""
+#lv3
+n,k=list(map(int,input().split()))
+ab=[]
+total=0
+ans=1
+p=[]
+for i in range(n):
+    a,b=list(map(int,input().split()))
+    if b <= k:
+        total+=b
+        ab.append([a,b])
+
+ab.sort(key=lambda x:x[0],reverse=True)
+
+for i in ab:
+    if total<=k:
+        print(total)
+        print("break")
+        break
+    else:
+        print(total)
+        ans=i[0]+1
+        total-=i[1]
+print(ab)
+print(ans)
