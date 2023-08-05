1d2c54087bf5fcb5334814f737063d969e04576e
Yoshi01O <22211370yy@tama.ac.jp>
2023-08-06 00:57:36 +0900

    たぶん解法見つけた

--- Py/2023_0729.py
+++ Py/2023_0729.py
@@ -22,25 +22,16 @@ for i in range(n-8):
             if [s[i+5][j+5:j+9],s[i+6][j+5:j+9],s[i+7][j+5:j+9],s[i+8][j+5:j+9]] == [[".",".",".","."],[".","#","#","#"],[".","#","#","#"],[".","#","#","#"]]:
                 print(i+1,j+1)
 """
-"""
-"""
 #lv3
 n,m=list(map(int,input().split()))
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 a.sort()
 
-for i in range(len(a)):
-    seller=i+1
+x=max(a)
+for i in b:
     buyer=0
-    for j in b:
-        if a[i] <= j:
-            buyer+=1
-    if max(b)+1 < min(a):
-        x=max(b)+1
-        break
-    elif seller >= buyer:
-        x=a[i]
-        break
+    if i >= x:
+        buyer+=1
 
 print(x)
