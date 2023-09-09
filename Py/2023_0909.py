e9c426ce7da70800f865f0e53f49699b46a1fa0a
Yoshi01O <22211370yy@tama.ac.jp>
2023-09-09 22:52:48 +0900

    確率わからん

---
+++ Py/2023_0909.py
@@ -0,0 +1,39 @@
+"""
+#lv1
+n=input()
+rank=["tourist","ksun48","Benq","Um_nik","apiad","Stonefeang","ecnerwala","mnbvmar","newbiedmy","semiexp"]
+ranknum=[3858,3679,3658,3648,3638,3630,3613,3555,3516,3481]
+ans="No"
+
+for i in range(len(rank)):
+    if n == rank[i]:
+        ans=ranknum[i]
+        break
+print(ans)
+"""
+"""
+#lv2
+n=int(input())
+ans=[]
+for i in range(n+1):
+    for j in range(9):
+        j+=1
+        if i%(n/j) == 0:
+            ans.append(j)
+            break
+    else:
+        ans.append("-")
+for i in ans:
+    print(i ,end="")
+"""
+#lv3
+import math
+c=[]
+for i in range(3):
+    c+=list(map(int,input().split()))
+print(c)
+all=math.perm(9,9)
+sad=math.perm(7,7)*(math.perm(3,3))
+ans=sad/all
+print(ans)
+print(sad)
