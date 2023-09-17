8e9c48601af64db657c3c058b37ac07df620f924
Yoshi01O <22211370yy@tama.ac.jp>
2023-09-18 00:20:30 +0900

    Update

---
+++ Py/2023_0917.py
@@ -0,0 +1,24 @@
+t=int(input())
+
+for _ in range(t):
+    ans="No"
+    baisu=set()
+    n=int(input())
+    for i in range(n//2):
+        i+=2
+        if n % i == 0:
+            baisu.add(i)
+        else:
+            continue
+    print(baisu)
+    a=[]
+    for i in baisu:
+        a.append(i)
+        if sum(a) == n:
+            if len(a) <= n:
+                ans="Yes"
+            else:
+                break
+    else:
+        ans="Yes"
+    print(ans)
