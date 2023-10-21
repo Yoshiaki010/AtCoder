592db3daf360116110648c30097355a13e06aebc
Yoshi01O <22211370yy@tama.ac.jp>
2023-10-21 22:42:59 +0900

    Updte

---
+++ Py/2023_1021.py
@@ -0,0 +1,16 @@
+#lv2
+n=int(input())
+wlis=[]
+xlis=[]
+
+for _ in range(n):
+    w,x=map(int,input().split())
+    wlis.append(w)
+    xlis.append(x)
+
+mintime=min(xlis)
+maxp=max(wlis)
+print(xlis)
+print(wlis)
+print(mintime)
+print(maxp)
