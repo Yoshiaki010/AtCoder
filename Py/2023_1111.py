8547ec7fc6c78c299cac57332f19a37d1f52a10f
Yoshi01O <22211370yy@tama.ac.jp>
2023-11-12 23:06:51 +0900

    TO_KE_NA_I

--- Py/2023_1111.py
+++ Py/2023_1111.py
@@ -26,20 +26,43 @@ print(ans)
 """
 #lv3
 """
-"""
 n,q=map(int,input().split())
 s=list(input())
 for _ in range(q):
     ans=0
     l,r=map(int,input().split())
     now = s[l-1:r]
-    left=["X"]
-    left+=now[0::2]
+    left=now[0::2]
     right=now[1::2]
-    print(left,right)
+    if n%2 == 0:
+        left+=["X"]
     for i in range(len(right)):
         if left[i] == right[i]:
             ans+=1
         if left[i+1] == right[i]:
             ans+=1
+    print(ans)
+"""
+n,q=map(int,input().split())
+s=list(input())
+anss=[]
+left=s[0::2]
+right=s[1::2]
+if n%2 == 0:
+    left+=["X"]
+for i in range(len(right)):
+    if left[i] == right[i]:
+        anss.append(1)
+    else:
+        anss.append(0)
+    if left[i+1] == right[i]:
+        anss.append(1)
+    else:
+        anss.append(0)
+print(anss)
+for _ in range(q):
+    ans=0
+    l,r=map(int,input().split())
+    now = anss[l-1:r-1]
+    ans= sum(now)
     print(ans)
