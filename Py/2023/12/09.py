da5335c717c3f5f1ad0d9a2506f347ae1e81cb85
Yoshi01O <22211370yy@tama.ac.jp>
2023-12-10 01:24:53 +0900

    NANDA

--- Py/2023/12/09.py
+++ Py/2023/12/09.py
@@ -3,38 +3,22 @@ n=int(input())
 a=list(map(int,input().split()))
 p=list(map(int,input().split()))
 ans="?"
-newa=a
-lastdif=0
-difdict={}
+lastdif=a[0]
 c={0}
 
-for i in range(n):
-    difdict[i]=0
-
+newa=a
 for i in range(n-1):
-    dif=a[p[i]-1]+a[i+1]
-    print(dif)
-    difdict[p[i]-1]+=dif
-    newa[p[i]-1]=dif
     if p[i]-1 in c:
         c.add(i+1)
-a=newa
-list(c)
-for i in c:
-    lastdif+=difdict[i]
-print(c)
-print(a)
-print(difdict)
-"""
-if num["+"] == num["-"]:
+        lastdif+=a[i+1]
+    newa[p[i]-1]=a[i+1]
+
+if lastdif == 0:
     ans=0
-elif num["+"] < num["-"]:
+elif lastdif < 0:
     ans="-"
 else:
     ans="+"
-"""
-#print(num)
-print(lastdif)
 print(ans)
 """
 """
