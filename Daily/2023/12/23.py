33e9d903d8a3906812e4e7adff5a5fb74ff3268d
Yoshi01O <22211370yy@tama.ac.jp>
2023-12-23 22:43:38 +0900

    Update

---
+++ Daily/2023/12/23.py
@@ -0,0 +1,27 @@
+"""
+#lv1
+b,g=map(int,input().split())
+pre="?"
+if b < g:
+  pre="Glove"
+else:
+  pre="Bat"
+print(pre)
+"""
+"""
+"""
+#lv2
+a,m,x,y=map(int,input().split())
+if x == y:
+    ans=0
+elif a < x:
+    now =x+(m-(x-a)%m)
+    ans =((y-now)//m)+1
+elif x <= a and a <= y:
+    ans=1
+    ans+=(a-x)//m
+    ans+=(y-a)//m
+else:
+    now =y-(m-(a-y)%m)
+    ans =((now-x)//m)+1
+print(ans)
