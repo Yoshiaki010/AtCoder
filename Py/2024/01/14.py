9db2d2a7ae256f7db2da2176b12fa61ad28145ef
Yoshi01O <22211370yy@tama.ac.jp>
2024-01-14 23:29:27 +0900

    多分2進数だな

--- Py/2024/01/14.py
+++ Py/2024/01/14.py
@@ -1,11 +1,32 @@
 #lv1
 """
+n=int(input())
+ans="L"+"o"*n+"ng"
+print(ans)
 """
-
 #lv2
 """
+n=int(input())
+ans=-1
+if n % 2 != 0:
+    ans=0
+elif n == 0:
+    ans=1
+else:
+    ans=0
+    while n % 2 == 0:
+        n//=2
+        ans+=1
+print(ans)
 """
-
 #lv3
 """
 """
+n=int(input())
+g=[0,2,4,6,8]
+ans=[]
+k=n//5
+while n > 4:
+    n/=5
+
+print(k)
