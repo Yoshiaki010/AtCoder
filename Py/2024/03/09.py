d5f2df94dbcec96f0ac93e506138374487a97f37
Yoshi01O <22211370yy@tama.ac.jp>
2024-03-10 00:03:14 +0900

    TLEでだめ

--- Py/2024/03/09.py
+++ Py/2024/03/09.py
@@ -24,24 +24,29 @@ for i in range(n):
     print(lista[n-1-i])
 """
 #lv3
-anslistn=int(input())
-anslist=list(map(int,input().split()))
-for _ in range(2):
-    n=int(input())
-    a=list(map(int,input().split()))
-    new=[]
-    newn=0
-    for i in range(n):
-        for j in range(anslistn):
-            new.append(a[i]+anslist[j])
-            newn+=1
-    anslist=new
-    anslistn=newn
+
+n=int(input())
+a=list(map(int,input().split()))
+m=int(input())
+b=list(map(int,input().split()))
+anslist=[]
+anslistn=0
+for i in range(n):
+    for j in range(m):
+        anslist.append(a[i]+b[j])
+        anslistn+=1
+l=int(input())
+c=list(map(int,input().split()))
 q=int(input())
 x=list(map(int,input().split()))
 for i in range(q):
-    if x[i] in anslist:
-        ans="Yes"
+    for j in range(anslistn):
+        y=x[i]-anslist[j]
+        if y in c:
+            ans="Yes"
+            break
+        else:
+            continue
     else:
         ans="No"
     print(ans)
