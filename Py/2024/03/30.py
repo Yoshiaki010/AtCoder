8ffc6fec9ed99753405f9de6a7e8b7e197ea74cc
Yoshi01O <22211370yy@tama.ac.jp>
2024-03-30 22:53:57 +0900

    lv3_close

---
+++ Py/2024/03/30.py
@@ -0,0 +1,51 @@
+#lv1 
+"""
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+for i in range(n):
+    if a[i] % k == 0:
+        print(a[i]//k,end=" ")
+    else:
+        continue
+"""
+#lv2
+"""
+s=input()
+n=len(s)
+ans=0
+lists=[]
+for i in range(n):
+    new=0
+    for j in range(n):
+        j+=1
+        if s[i:j] not in lists and i != j and s[i:j] != "":
+            lists.append(s[i:j])
+            new+=1
+        else:
+            continue
+    ans+=new
+print(ans)
+"""
+"""
+"""
+#lv3
+n,a,b=map(int,input().split())
+d=list(map(int,input().split()))
+ans="Unknow"
+holid=[-d[0],a-d[0]]
+for i in range(n-1):
+    s=d[i+1]-d[0]
+    w=d[i+1]//(d[0]+a+b)
+    p=d[i+1]%(d[0]+a+b)
+    print(p)
+"""
+#    next=(d[0]+a+b)*w
+    print(s,holid[0]+next,holid[1]+next)
+    if s < holid[0]+next or holid[1]+next < s:
+        ans="No"
+        break
+    else:
+        continue
+else:
+    ans="Yes"
+print(ans)"""
