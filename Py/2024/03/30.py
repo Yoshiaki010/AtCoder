551d36299923b2220e1cf56a0ffd11200974a7b4
Yoshi01O <22211370yy@tama.ac.jp>
2024-04-21 00:03:13 +0900

    Update

--- Py/2024/03/30.py
+++ Py/2024/03/30.py
@@ -34,15 +34,6 @@ d=list(map(int,input().split()))
 ans="Unknow"
 holid=[0,a+1]
 for i in range(n-1):
-<<<<<<< HEAD
-    s=d[i+1]-d[0]
-    w=d[i+1]//(d[0]+a+b)
-    p=d[i+1]%(d[0]+a+b)
-    print(p)
-#    next=(d[0]+a+b)*w
-    print(s,holid[0]+next,holid[1]+next)
-    if s < holid[0]+next or holid[1]+next < s:
-=======
     now=d[i+1]-d[0]
     w=d[i+1]//(a+b)
     next=(a+b)*w
@@ -50,13 +41,8 @@ for i in range(n-1):
         print(now)
         continue
     else:
->>>>>>> dd162713a7875b1f149f554a57facf829a0a65b8
         ans="No"
         break
 else:
     ans="Yes"
-<<<<<<< HEAD
 print(ans)
-=======
-print(ans)
->>>>>>> dd162713a7875b1f149f554a57facf829a0a65b8
