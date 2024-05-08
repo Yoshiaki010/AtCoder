b502e4bf26bf301f32a7f19a36a859057b1a45f0
Yoshi01O <22211370yy@tama.ac.jp>
2024-05-09 02:09:54 +0900

    lv2

---
+++ Py/2016/07/23.py
@@ -0,0 +1,35 @@
+#lv1
+"""
+a,b,c=map(int,input().split())
+ans="Unknow"
+if a==7:
+    if b==5 and c==5:
+        ans="YES"
+    else:
+        ans="NO"
+elif b==7:
+    if a==5 and c==5:
+        ans="YES"
+    else:
+        ans="NO"
+elif c==7:
+    if a==5 and b==5:
+        ans="YES"
+    else:
+        ans="NO"
+else:
+    ans="NO"
+print(ans)
+"""
+#lv2
+"""
+"""
+n,e=map(int,input().split())
+s=[]
+for _ in range(n):
+    s.append(input())
+
+
+#lv3
+"""
+"""
