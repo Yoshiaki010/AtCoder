c7592e8ef3e93723f3558b672f3a75bb27da6fba
Yoshi01O <22211370yy@tama.ac.jp>
2024-05-19 23:15:22 +0900

    Udpate

---
+++ Py/2024/05/19.py
@@ -0,0 +1,14 @@
+#lv4
+"""
+"""
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+p=""
+for i in range(n):
+    p+=str(i+1)
+print(p)
+ans=""
+for i in range(n-1):
+    if p[i]+1 == p[i+1] or p[i]-1 == p[i+1]:
+        ans+=str(p[i])+" "
+#lv5  
