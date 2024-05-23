e8d65f7bc53c5b9b54be2fac3550ddc8d11a37af
Yoshiaki <22211370yy@tama.ac.jp>
2024-05-23 19:32:17 +0900

    Update

---
+++ Daily/2024/05/23-1830.py
@@ -0,0 +1,36 @@
+#lv2
+"""
+n=int(input())
+a=list(map(int,input().split()))
+mt=[]
+for _ in range(n-1):
+    mt.append(list(map(int,input().split())))
+for i in range(n-1):
+    h=a[i]//mt[i][0]
+    a[i]%=mt[i][0]
+    a[i+1]+=h*mt[i][1]
+print(a[n-1])
+""" 
+#lv2
+"""
+n=str(bin(int(input())))
+m=len(n)-1
+ans=0
+for i in range(m-1):
+    if n[m-i] == "0":
+        ans+=1
+    else:
+        break
+print(ans)
+""" 
+#lv3
+"""
+""" 
+
+#lv3
+"""
+""" 
+
+#lv4
+"""
+""" 
