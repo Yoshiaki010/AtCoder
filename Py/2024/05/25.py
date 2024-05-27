33c8fffaefb3cda1c3c18308e927586dc55e3cb2
Yoshiaki <22211370yy@tama.ac.jp>
2024-05-27 17:06:59 +0900

    Update

--- Py/2024/05/25.py
+++ Py/2024/05/25.py
@@ -19,67 +19,44 @@ else:
 print(ans)
 """ 
 #lv2
+"""
 n,m=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 dica={}
-for i in range(n):
-    dica[a[i]] = []
-for i in range(n-1):
-    dica[a[i]] += [a[i+1]]
-    dica[a[i+1]] += [a[i]]
+
 c=a+b
 c.sort()
 
 ans="No"
 for i in range(n+m-1):
     if c[i] in a and c[i+1] in a:
-        if c[i+1] in set(dica[c[i]]):
-            ans="Yes"
-            break
-        else:
-            continue
+        ans="Yes"
+        break
     else:
         continue
 else:
     ans="No"
 print(ans)
 """
-a.sort()
-for i in range(n-1):
-    if a[i]+1 == a[i+1]:
-        ans="Yes"
-        break
-    else:
-        ans="No"
-print(ans)
-"""
 
 #lv3
 """
+"""
 n,t=map(int,input().split())
 a=list(map(int,input().split()))
-c=[0]*(n*n)
 ans=0
+tate=[0]*n
+yoko=[0]*n
+naname=[0]*2
 for i in range(t):
-    c[a[i]-1]=1
-    for j in range(3):
-        tate=c[j::n]
-        yoko=c[j*n:j*n+n]
-        if set(tate) == {1} or set(yoko) == {1}:
-            ans=i+1
-    nanamex=c[0::n+1]
-    nanamey=c[n-1:(n-1)*n+1:n-1]
-    if set(nanamex) == {1} or set(nanamey) == {1}:
-        ans=i+1
-    if ans != 0:
-        break
+    tate[a[i]%n]+=1
+    yoko[a[i]%n]+=1
+    if a == a:
+        naname[0]+=1
     else:
-        continue
-else:
-    ans=-1
+        naname[1]+=1
 print(ans)
-"""
 
 #lv4
 """
