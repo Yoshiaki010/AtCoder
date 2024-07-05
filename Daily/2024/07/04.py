c0ca8acb30a676f95403486d6983ed81c4c9bd3b
Yoshi01O <22211370yy@tama.ac.jp>
2024-07-05 23:46:06 +0900

    clean

--- Daily/2024/07/04.py
+++ Daily/2024/07/04.py
@@ -18,31 +18,34 @@ for i in range(n):
 print(ans)
 """
 #lv3
-"""
-"""
 n=int(input())
-num=set()
-data=[]
+keyset=set()
+hashigolist=[]
 for i in range(n):
     a,b=map(int,input().split())
-    data.append([a,b])
-    num.add(a)
-    num.add(b)
+    hashigolist.append([a,b])
+    keyset.add(a)
+    keyset.add(b)
 
-numlist=list(num)
+keylist=list(keyset)
 
-hashigo=dict()
-hashigo[1]=[]
-for i in range(len(numlist)):
-    hashigo[numlist[i]]=[]
+hashigodict=dict()
+hashigodict[1]=[]
+for i in range(len(keylist)):
+    hashigodict[keylist[i]]=[]
 
 for i in range(n):
-    a,b=data[i]
-    hashigo[a].append(b)
-    hashigo[b].append(a)
+    a,b=hashigolist[i]
+    hashigodict[a].append(b)
+    hashigodict[b].append(a)
 
-allnexts=[]
+print(hashigolist)
+print(hashigodict)
+
+worked=set()
 nexts=[1]
+ans=0
+"""
 while 0 < nexts:
     newnext=set()
     for now in nexts:
@@ -53,7 +56,9 @@ while 0 < nexts:
             newnext.add(next)
     next.append(hashigo[now])
     hashigo[now]=[]
+
 print(ans)
+"""
 #lv3
 """
 """
