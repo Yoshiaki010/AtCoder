b95c910287c95888cd417fc5e9d94a6c900bffa9
Yoshi01O <22211370yy@tama.ac.jp>
2024-07-13 03:54:31 +0900

    Update

--- Daily/2024/07/04.py
+++ Daily/2024/07/04.py
@@ -50,17 +50,55 @@ print(ans)
 #lv3
 """
 """
-h,w=map(int,input().split())
+ah,aw=map(int,input().split())
 a=[]
-for _ in range(h):
+for _ in range(ah):
     a.append(list(map(int,input().split())))
-h,w=map(int,input().split())
+
+
+bh,bw=map(int,input().split())
 b=[]
-for _ in range(h):
-    b.append(list(map(int,input().split())))
-print(a)
+for _ in range(bh):
+    b+=list(map(int,input().split()))
+
+for i in range(ah):
+    print(a[i])
 print(b)
 
+sameplace=[]
+samehw=0
+ans="Unknow"
+for i in range(ah):
+    for j in range(aw):
+        if samehw == bh*bw:
+            break
+        else:
+            if a[i][j] == b[samehw]:
+                sameplace.append(j)
+                samehw+=1
+            else:
+                continue
+    else:
+        continue
+    break
+else:
+    ans="No"
+
+ans=[]
+print(sameplace)
+
+for i in range(bw-1):
+    samei=sameplace[i::bw]
+    samej=sameplace[i+1::bw]
+    print(set(samei),set(samej))
+    if len(set(samei)) == 1  and len(set(samej)) == 1 and samei[0] < samej[0]:
+            continue
+    else:
+        ans="No"
+        break
+else:
+    ans="Yes"
+print(ans)
 #lv4
 """
 """
