51bd7b31a82aaa21e1da5b40647b0872a313dffd
Yoshi01O <22211370yy@tama.ac.jp>
2024-08-14 23:17:04 +0900

    lv2,3解けた

--- Py/2024/08/10.py
+++ Py/2024/08/10.py
@@ -12,7 +12,6 @@ print(ans)
 """
 #lv2
 """
-"""
 n=int(input())
 alls=[]
 allm=[]
@@ -20,50 +19,48 @@ for _ in range(n):
     s=input()
     alls.append(s)
     allm.append(len(list(s)))
-alls.append(alls[0])
+allm.append(allm[0])
 
 t=[]
 m=max(allm)
 for i in range(m):
     t.append("")
 
-print(alls,allm)
-
 for i in range(n):
-    si=allm[i]
     for j in range(m):
-        if -1 < j and j < si:
-            t[j]+=alls[i][j]
+        if -1 < j and j < allm[i]:
+            t[j]=alls[i][j]+t[j]
         else:
-            if si-1 < j and j < m:
-                t[j]+="*"
+            if j < allm[i-1]:
+                t[j]="*"+t[j]
+                allm[i]+=1
             else:
                 break
 
 for i in range(m):
     print(t[i])
+"""
 
 #lv3 
 """
-    print("Q:",x)
-    print("bag:",bag)
 q=int(input())
-bag=dict()
-bagnum=0
+balls=dict()
+ballkindnum=0
 for _ in range(q):
     x=input().split()
     if x[0] == "1":
-        if x[1] not in bag:
-            bag[x[1]]=1
-            bagnum+=1
+        if x[1] not in balls:
+            balls[x[1]]=1
         else:
-            bag[x[1]]+=1
+            balls[x[1]]+=1
+        if balls[x[1]] == 1:
+            ballkindnum+=1
     elif x[0] == "2":
-        bag[x[1]]-=1
-        if bag[x[1]]==0:
-            bagnum-=1
+        balls[x[1]]-=1
+        if balls[x[1]]==0:
+            ballkindnum-=1
         else:
             pass
     else:
-        print(bagnum)
+        print(ballkindnum)
 """
