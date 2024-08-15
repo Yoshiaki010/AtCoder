e5525f1893bc3da32103f8f834a128f68956b2ab
Yoshi01O <22211370yy@tama.ac.jp>
2024-08-15 23:57:18 +0900

    MUZU

---
+++ Daily/2024/08/15.py
@@ -0,0 +1,27 @@
+#lv2
+"""
+b=int(input())
+ans=b**(1/3)
+print(ans)
+"""
+#lv2
+"""
+"""
+s=int(input())
+print(s)
+line=["False" , "False" , "False"]
+for i in range(10):
+    if i == 0 and s[i] == 0:
+
+#lv3
+"""
+"""
+
+#lv3
+"""
+"""
+
+#lv4
+"""
+"""
+
