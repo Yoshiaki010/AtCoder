dc796cc3f5d46d745265afff0cdab4cde10a1882
Yoshi01O <22211370yy@tama.ac.jp>
2024-08-17 02:53:48 +0900

    TOKENAI

--- Daily/2024/08/15.py
+++ Daily/2024/08/15.py
@@ -7,12 +7,41 @@ print(ans)
 #lv2
 """
 """
-s=int(input())
-print(s)
-line=["False" , "False" , "False"]
-for i in range(10):
-    if i == 0 and s[i] == 0:
+s=list(map(int,list(input())))
+fast=False
+lines=[False,False,False,False,False,False,False]
+if s[0] == 0:
+    fast = True
+if s[0]+s[4] == 0:
+    lines[3] = True
+elif s[1]+s[7] == 0:
+    lines[2] = True
+elif s[2]+s[i+6] == 0:
+    lines[4] = True
+elif i == 3 and s[i] == 0:
+    lines[1] = True
+elif i == 5 and s[i] == 0:
+    lines[5] = True
+elif i == 6 and s[i] == 0:
+    lines[0] = True
+elif i == 9 and s[] == 0:
+    lines[6] = True
+else:
+    continue
+print(lines)
+print(fast)
+ans="Unknow"
+if fast:
+    for i in range(4):
+        if lines[i] and not lines[i+1] and lines[i+2]:
+            ans="Yes"
+            break
+        else:
+            continue
+else:
+    ans="No"
 
+print(ans)
 #lv3
 """
 """
