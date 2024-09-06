7fc4e2e332f27387a134289d241b4f5e03b28417
Yoshi01O <22211370yy@tama.ac.jp>
2024-09-06 23:15:15 +0900

    lv2 TOKETA

--- Daily/2024/08/15.py
+++ Daily/2024/08/15.py
@@ -17,23 +17,29 @@ print(ans)
 """
 """
 s=list(map(int,list(input())))
-lines=[True]*7
+top=s[0]
+lines=[s[6],s[3],s[1]+s[7],s[0]+s[4],s[2]+s[8],s[5],s[9]]
 print(lines)
-top=False
-for i in range(7):
-    if i == 0:
-        if s[i] == 0:
-            top = False
-            if s[4] == 0:
-                lines[0] = False
-            else:
-                lines[True]
+ans="unknow"
+if top == 0:
+    for i in range(7):
+        for j in range(7-(i+1)):
+            j+=i
+            print(lines[i],lines[j],lines[i+1:j+1])
+            if lines[i] > 0 and lines[j] > 0:
+                for k in range(j-i):
+                    if lines[i+k] < 1:
+                        ans="Yes"
+                        print("break1")
+                        break
+                    else:
+                        print("continu1")
+                        ans="No"
+                        continue
         
-        if s[i]
-    elif i == 1
-    else:
-        lines[i] = True
-
+print("fin")        
+                
+print(ans)
 #lv3
 """
 """
