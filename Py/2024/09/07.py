64b9b2127c3e4a6b82c9f9b7dcf61c0eb5bf012b
Yoshi01O <22211370yy@tama.ac.jp>
2024-09-07 22:45:38 +0900

    lv4どうとくべきか...

--- Py/2024/09/07.py
+++ Py/2024/09/07.py
@@ -1,11 +1,65 @@
 #lv1 
 """
+l,r=map(int,input().split())
+ans="Unknow"
+if l+r == 1:
+    if l == 1:
+        ans="Yes"
+    else:
+        ans="No"
+else:
+    ans="Invalid"
+print(ans)
 """
 
 #lv2
 """
+n=int(input())
+a=[]
+for _ in range(n):
+    a.append(list(map(int,input().split())))
+
+i=0
+for j in range(n):
+    if j-1 < i:
+        i=a[i][j]-1
+    else:
+        i=a[j][i]-1
+print(i+1)
 """
 
 #lv3
 """
 """
+#print(wrong_place)
+#print(n)
+s=list(input())
+t=list(input())
+n=len(s)
+wrong=0
+wrong_place=[]
+for i in range(n):
+    if s[i] == t[i]:
+        continue
+    else:
+        wrong+=1
+        wrong_place.append(i)
+print(wrong)
+for _ in range(wrong):
+    now=wrong_place[0]
+    wrong_place=wrong_place[1:]
+    snum=ord(s[now])
+    tnum=ord(t[now])
+    if snum < tnum:
+        wrong_place.append(now)
+    else:
+        s[now] = t[now]
+        wrong-=1
+        print("".join(s))
+wrong_place.sort(reverse=True)
+for _ in range(wrong):
+    now=wrong_place[0]
+    wrong_place=wrong_place[1:]
+    wrong-=1
+    s[now] = t[now]
+    print("".join(s))
