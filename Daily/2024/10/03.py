7c3d552b0f3e22602b6859e74d9a5f8d537e8f16
Yoshi01O <22211370yy@tama.ac.jp>
2024-10-04 01:33:46 +0900

    解法考え中

--- Daily/2024/10/03.py
+++ Daily/2024/10/03.py
@@ -1,1 +1,34 @@
-a 
+#lv2
+"""
+n,x=map(int,input().split())
+a=list(map(int,input().split()))
+
+iknow=[0]*n
+next=x
+ans=0
+for _ in range(n):
+    if 0 == iknow[next-1]:
+        iknow[next-1] += 1
+        ans += 1
+        next = a[next-1]
+    else:
+        break
+print(ans)
+"""
+#lv2
+"""
+"""
+n,m=map(int,input().split())
+s=list(map(int,input().split()))
+wrong=[0]*n
+#lv3
+"""
+"""
+
+#lv3
+"""
+"""
+
+#lv4
+"""
+"""
