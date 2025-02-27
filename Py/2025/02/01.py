6b4cf697bad476804c4d5417fd388af892bdf2e8
Yoshi01O <22211370yy@tama.ac.jp>
2025-02-28 05:54:03 +0900

    lv3

--- Py/2025/02/01.py
+++ Py/2025/02/01.py
@@ -15,7 +15,6 @@ print(ans)
 """
 #lv2
 """
-"""
 n,m = map(int,input().split())
 s = []
 for i in range(n):
@@ -26,22 +25,49 @@ for i in range(m):
     t.append(input())
 
 ans = []
-for i in range(n-1):
-    for j in range(n-1):
+judge = "No"
+for i in range(n-m+1):
+    for j in range(n-m+1):
         if s[i][j] == t[0][0]:
-            for ti in range(m):
-                for tj in range(m):
-                    if s[i+ti][j+tj] == t[ti][tj]:
+            for k in range(m):
+                for l in range(m):
+                    if s[i+k][j+l] == t[k][l]:
                         continue
                     else:
+                        judge = "No"
                         break
                 else:
                     continue
                 break
             else:
+                judge = "Yes"
                 ans = [i+1,j+1]
-                break
+        else:
+            judge = "No"
+        if judge == "Yes":
+            break
+        else:
+            continue
 print(*ans)
+"""
 #lv3
 """
+N,Q = map(int,input().split())
+nest = [1 for _ in range(N)]
+bird = [i+1 for i in range(N)]
+
+ans = 0
+for i in range(Q):
+    query = input()
+    if query[0] == "1":
+        n,p,h = map(int,query.split())
+        if nest[bird[p-1]-1] == 2:
+            ans -= 1
+        if nest[h-1] == 1:
+            ans += 1
+        nest[bird[p-1]-1] -= 1
+        bird[p-1] = h
+        nest[bird[p-1]-1] += 1
+    else:
+        print(ans)
 """
