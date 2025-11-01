fe666638eace980b01f73753c5563698b7762729
yoshi2 <22211370yy@tama.ac.jp>
2025-11-01 20:11:23 +0900

    Update

--- Py/2025/10/25.py
+++ Py/2025/10/25.py
@@ -1,10 +1,26 @@
 #lv1
 """
+N,M = map(int,input().split())
+for i in range(1,N  + 1):
+  if i < M + 1:
+    ans = "OK"
+  else:
+    ans = "Too Many Requests"
+  print(ans)
+"""
+#oneline
+"""
+ans = [print("OK") if i <= x[1] else print("Too Many Requests") for x in [list(map(int,input().split()))] for i in range(1, x[0] + 1) ]
 """
 
 #lv2
 """
 """
+for i in range(input()):
+    if i == 2:
+        break
+else:
+    print("")
 
 #lv3
 """
