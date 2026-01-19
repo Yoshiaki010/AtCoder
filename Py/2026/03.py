c2818c438b0ef459ab9a39bbe8fa33d373dd449d
Yoshiaki <22211370yy@tama.ac.jp>
2026-01-19 13:04:32 +0900

    Update

--- Py/2026/03.py
+++ Py/2026/03.py
@@ -4,36 +4,41 @@ N = int(input())
 ans = 2 ** N - 2 * N
 print(ans)
 """
+
 #lv2
 """
-"""
-#int(input())
-yes = []
-no = []
-for i in range(1,2026):
-    N = i
-    print(N, end = " = ")
+def cal(x):
+    lis_x = list(map(int,list(str(x))))
+    next = 0
+    for i in range(len(lis_x)):
+        next += lis_x[i] * lis_x[i]
+    return next
 
-    next = list(str(N))
-    next_int = N
-    for _ in range(10):
-        if next_int == 1:
-            yes.append(N)
-            break
-        else:
-            next_int = 0
-            for i in range(len(next)):
-                next_int += int(next[i]) ** 2
-            next = list(str(next_int))
+N = int(input())
+
+next_int = 0
+for _ in range(2):
+    next_int = cal(N)
+
+ans = "Unknow"
+for _ in range(243):
+    next_int = cal(next_int)
+
+    if next_int == 1:
+        ans = "Yes"
+        break
     else:
-        no.append(N)
-    print(next_int)
+        continue
+else:
+    ans = "No"
 
-print(yes[:100],len(yes))
-print(no[:100],len(no))
+print(ans)
+"""
+
 #lv3
 """
 """
+
 
 #lv4
 """
