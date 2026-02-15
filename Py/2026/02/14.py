04e3776e81b1c96f30e3d8e45bb17b9dcb6ffc35
Yoshi01O <22211370yy@tama.ac.jp>
2026-02-15 18:05:37 +0900

    制約よくみよう

--- Py/2026/02/14.py
+++ Py/2026/02/14.py
@@ -36,65 +36,18 @@ for i in range(N):
 
 #lvC
 """
-"""
 N = int(input())
 A = list(map(int,input().split()))
 
+dis_loop = [0 for _ in range(N)]
+
 for i in range(N):
-    s = i
-    next = 0
-    loop_s = 0
-    walked = set()
-    for _ in range(N):
-        if s not in walked:
-            next = A[s] - 1
-            walked.add(s)
-            s = next
-        else:
-            loop_s = s
-            break
+    back_i = N - i - 1
 
-#    print(loop_s, walked)
+    if A[back_i] == N - i:
+        dis_loop[back_i] = N - i
+    else:
+        dis_loop[back_i] = dis_loop[A[back_i] - 1]
 
-    loop_n = 0
-    s = loop_s
-    next = 0
-    for _ in range(N):
-        next = A[s] - 1
-        loop_n += 1
-        if loop_s == next:
-            break
-        else:
-            continue
-
-#    print(loop_n)
-
-    k = 10 ** 100
-#    print(k)
-
-    s = i
-    next = 0
-    for _ in range(N):
-#        print(f"walk to {s}")
-        next = A[s] - 1
-        s = next
-        k -= 1
-        if next == loop_s:
-            break
-        else:
-            continue
-
-#    print(s,k)
-
-    k = k % loop_n
-
-#    print(k)
-
-    s = loop_s
-    next = 0
-    for _ in range(k):
-        next = A[s] - 1
-        s = next
-
-    ans = s
-    print(ans + 1, end = " ")
+print(*dis_loop)
+"""
