46efe114a2155a08f062b070d87cd7845c43874a
Yoshi01O <22211370yy@tama.ac.jp>
2026-04-11 22:59:16 +0900

    Update

--- Py/2026/04/11.py
+++ Py/2026/04/11.py
@@ -1,9 +1,37 @@
 #lvA
 """
+N = int(input())
+S = input()
+
+ans = []
+head = "Unknow"
+if S[0] == "o":
+    head = True
+else:
+    head = False
+
+for i in range(N):
+    if S[i] == "o" and head:
+        continue
+    else:
+        head = False
+        ans.append(S[i])
+
+print("".join(ans))
 """
 
 #lvB
 """
+T,X = map(int,input().split())
+A = list(map(int,input().split()))
+
+t = 0
+front_A = 0
+for i in range(T + 1):
+    if X <= abs(front_A - A[i]):
+        front_A = A[i]
+        print(f"{t} {front_A}")
+    t += 1
 """
 
 #lvC
